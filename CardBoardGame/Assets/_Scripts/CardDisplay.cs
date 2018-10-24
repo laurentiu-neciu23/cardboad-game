6920af4906144ebb137eed0a15f521a868457c0d
Daniel Pirvan <d_pirvan@yahoo.com>
2018-10-24 10:33:39 +0300

    Template pentru Carti Player
    (7/58 carti player adaugate)

---
+++ CardBoardGame/Assets/_Scripts/CardDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour {
+
+    public Text Attack;
+    public Text Defense;
+    public Text Name;
+    public Text Description;
+    public Text Team;
+
+    public PlayerCard card;
+
+	// Use this for initialization
+	void Start () {
+        UpdateDisplay();
+	}
+
+    private void Update()
+    {
+        UpdateDisplay();
+    }
+
+
+    public void UpdateDisplay()
+    {
+        Name.text = card.PlayerName;
+        Attack.text = card.AttackPoints.ToString();
+        Defense.text = card.DefensePoints.ToString();
+        Description.text = card.Description;
+        Team.text = card.Team;
+    }
+}
---
+++ CardBoardGame/Assets/_Scripts/PlayerCard.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Card", menuName = "Cards/Player Card")]
+public class PlayerCard : ScriptableObject {
+
+    [Header("Player Stats")]
+    public string PlayerName;
+    public int AttackPoints;
+    public int DefensePoints;
+    public string Description;
+    public string Team;
+    [Header("Position: A or M or F (atacant, mijlocas sau fundas)")]
+    public string position;
+
+
+
+
+    public Sprite artwork;
+
+
+	// Use this for initialization
+	void TakeAttack (int amount) {
+        AttackPoints -= amount;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
